4b2068f40b749c5c78f9b5900495d85fd34225af
realkhanghong <121533931+realkhanghong@users.noreply.github.com>
2025-05-16 09:52:39 +0700

    proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -2,7 +2,7 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c16c16
+    tr </dev/urandom -dc A-Za-z0-9 | head -c16
     echo
 }
 
@@ -82,8 +82,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal IP = ${IP4}. External sub for IP6 = ${IP6}"
 
-FIRST_PORT=30000
-LAST_PORT=35000
+FIRST_PORT=32000
+LAST_PORT=37000
 
 gen_data >$WORKDIR/data.txt
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
