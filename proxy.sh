97b09b8efcd0832fbd81a4f41a34a7049a6b8abe
realkhanghong <121533931+realkhanghong@users.noreply.github.com>
2025-05-16 10:02:58 +0700

    proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -82,8 +82,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal IP = ${IP4}. External sub for IP6 = ${IP6}"
 
-FIRST_PORT=38000
-LAST_PORT=40000
+FIRST_PORT=30000
+LAST_PORT=32000
 
 gen_data >$WORKDIR/data.txt
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
