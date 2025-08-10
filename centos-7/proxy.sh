db4b029eaec6784fe5848d01e2c8e85b96cb364f
realkhanghong <121533931+realkhanghong@users.noreply.github.com>
2025-08-10 11:36:34 +0700

    Update proxy.sh

--- centos-7/proxy.sh
+++ centos-7/proxy.sh
@@ -81,7 +81,7 @@ install_3proxy
 echo "working folder = /home/bkns"
 WORKDIR="/home/bkns"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
+mkdir -p $WORKDIR && cd $WORKDIR
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
