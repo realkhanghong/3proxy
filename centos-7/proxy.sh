60ea32013e2642b711d9c3a967d9f69db2f775c2
realkhanghong <121533931+realkhanghong@users.noreply.github.com>
2025-08-10 11:27:42 +0700

    Update proxy.sh

--- centos-7/proxy.sh
+++ centos-7/proxy.sh
@@ -15,15 +15,12 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    URL="https://github.com/z3APA3A/3proxy/archive/refs/tags/0.8.13.tar.gz"
+    wget -qO- $URL | tar -xzf-
+    cd 3proxy-0.8.13
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
-    #cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    #chmod +x /etc/init.d/3proxy
-    #chkconfig 3proxy on
     cd $WORKDIR
 }
 
@@ -112,6 +109,6 @@ bash /etc/rc.local
 
 gen_proxy_file_for_user
 rm -rf /root/setup.sh
-rm -rf /root/3proxy-3proxy-0.8.6
+rm -rf /root/3proxy-3proxy-0.8.13
 
 echo "Starting Proxy"
