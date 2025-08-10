b096f2bd0b54fccebaa866df7228cb519ad5a87d
realkhanghong <121533931+realkhanghong@users.noreply.github.com>
2025-08-10 11:18:08 +0700

    Update proxy.sh

--- centos-7/proxy.sh
+++ centos-7/proxy.sh
@@ -2,7 +2,7 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	tr </dev/urandom -dc A-Za-z0-9 | head -c16
 	echo
 }
 
@@ -91,8 +91,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=22000
-LAST_PORT=22700
+FIRST_PORT=30001
+LAST_PORT=32000
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
